8ef339e225d68e5338a3e0868992dfa575228a60
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-10-12 20:25:23 -0500

    Routes added for all pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import {BrowserRouter as Router} from "react-router-dom";
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Portfolio } from "./pages/Portfolio";
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
 
 function App() {
   return (
     <Router>
       <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="/portfolio" element={<Portfolio />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/contact" element={<Contact />}/>
       </Routes>
     </Router>
   );
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Portfolio(){
+    return (
+        <div>
+            <h2>Portfolio</h2>
+        </div>
+    )
+};
