cad95a2d0981e4098f459f914c93006e0972bb07
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-10-15 21:45:19 -0500

    responsive home page logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavBar } from "../utils/NavBar";
-import {ReactComponent as NameLogo} from "../images/lydia-ritchie-name-logo.svg"
+import { ReactComponent as NameLogo } from "../images/lydia-ritchie-name-logo.svg";
+import { ReactComponent as LongNameLogo } from "../images/lydia-ritchie-horizontal.svg";
 
 
 export function Home(){
@@ -8,7 +9,8 @@ export function Home(){
         <div>
             <header>{<NavBar/>}</header>
             <div className="py-3 d-flex row justify-content-center ">
-            <NameLogo className="w-75"/>
+                <LongNameLogo className="w-75  d-none d-sm-block"/>
+                <NameLogo className="w-75 d-sm-none"/>
                 </div>
                 
         </div>
