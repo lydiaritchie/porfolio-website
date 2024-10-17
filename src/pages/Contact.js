5ec29595c3d2ace70926e283b085b8e65b39834d
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-10-17 11:39:15 -0500

    Added random colors on hover contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,18 +1,31 @@
 import React from "react";
+import { useState } from "react";
 import linkedInIcon from "../images/linkedIn-icon.svg";
 import githubIcon from "../images/github-icon.svg";
 import emailIcon from "../images/email-bg-icon.svg";
 import instaIcon from "../images/insta-icon.svg";
 
 export function Contact() {
+    const colors = ["#D5F2E3", "#FCEADE", "#A4BFEB", "#AF9BB6", "#CDFFF9", "#FADF63", "#A3BBAD", "#FCAA67", "#AEADF0", "#EBB9DF"];
+    const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
+    const [linkedInColor, setLinkedInColor] = useState("transparent");
+    const [gitHubColor, setGitHubColor] = useState("transparent");
+    const [emailColor, setEmailColor] = useState("transparent");
+    const [instaColor, setInstaColor] = useState("transparent");
+
+
+
   return (
     <div>
       <h1 className="text-center py-3">Contact</h1>
       <div className="d-flex flex-wrap justify-content-center">
-        <div className="row my-3 w-75" style={{ backgroundColor: "" }}>
+        <div className="row my-3 w-75" >
           <a
             className="btn contact-btn"
             href="https://www.linkedin.com/in/lydia-g-ritchie/"
+            onMouseEnter={() => setLinkedInColor(getRandomColor())}
+            onMouseLeave={() => setLinkedInColor("transparent")}
+            style={{ backgroundColor: linkedInColor }}
           >
             <img
               className="mx-2"
@@ -26,6 +39,9 @@ export function Contact() {
           <a
             className="btn contact-btn"
             href="https://github.com/lydiaritchie"
+            onMouseEnter={() => setGitHubColor(getRandomColor())}
+            onMouseLeave={() => setGitHubColor("transparent")}
+            style={{ backgroundColor: gitHubColor }}
           >
             <img
               className="mx-2"
@@ -39,6 +55,9 @@ export function Contact() {
           <a
             className="btn contact-btn"
             href="mailto:lydia.g.ritchie@gmail.com"
+            onMouseEnter={() => setEmailColor(getRandomColor())}
+            onMouseLeave={() => setEmailColor("transparent")}
+            style={{ backgroundColor: emailColor }}
           >
             <img
               className="mx-2"
@@ -52,6 +71,9 @@ export function Contact() {
           <a
             className="btn contact-btn"
             href="https://www.instagram.com/lydiaritchieart/"
+            onMouseEnter={() => setInstaColor(getRandomColor())}
+            onMouseLeave={() => setInstaColor("transparent")}
+            style={{ backgroundColor: instaColor }}
           >
             <img
               className="mx-2"
