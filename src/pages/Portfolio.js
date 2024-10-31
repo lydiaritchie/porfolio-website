197858e390822af338a271adc06fa3248bcef62b
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-10-31 16:00:26 -0500

    Added projectData.js and mapped cards

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,13 +1,33 @@
 import React from "react";
 import { NavBar } from "../utils/NavBar";
+import { softwareEngineeringProjects, designProjects, potteryProjects } from "../utils/projectsData";
 
 export function Portfolio() {
+  // Map project data into cards
+  const softwareEngineeringCards = softwareEngineeringProjects.map(
+    (project) => {
+      return (
+      <div className="card my-2">
+        <div className="card-body">
+          <div className="card-title">{project.title}</div>
+          <p className="card-text">Card text</p>
+        </div>
+      </div>)
+    }
+  );
+
   return (
-    <div>
+    <div className="main-container">
       <header>
         <NavBar />
       </header>
-      <h5 className="text-center py-3">Portfolio</h5>
+      <h5 className="text-center">Portfolio</h5>
+
+      <div className="d-flex row m-3">
+        <h6>Software Engineering</h6>
+        
+        {softwareEngineeringCards}
+        </div>
     </div>
   );
 }
---
+++ src/utils/projectsData.js
@@ -0,0 +1,53 @@
+const softwareEngineeringProjects = [
+  {
+    id: 1,
+    title: "Project One",
+    description: "A description of the first project.",
+    image: "path/to/image1.jpg",
+    url: "https://example.com/project-one",
+  },
+  {
+    id: 2,
+    title: "Project Two",
+    description: "A description of the second project.",
+    image: "path/to/image2.jpg",
+    url: "https://example.com/project-two",
+  },
+  // Add more projects as needed
+];
+
+const designProjects = [
+  {
+    id: 1,
+    title: "Project One",
+    description: "A description of the first project.",
+    image: "path/to/image1.jpg",
+    url: "https://example.com/project-one",
+  },
+  {
+    id: 2,
+    title: "Project Two",
+    description: "A description of the second project.",
+    image: "path/to/image2.jpg",
+    url: "https://example.com/project-two",
+  },
+];
+
+const potteryProjects = [
+  {
+    id: 1,
+    title: "Project One",
+    description: "A description of the first project.",
+    image: "path/to/image1.jpg",
+    url: "https://example.com/project-one",
+  },
+  {
+    id: 2,
+    title: "Project Two",
+    description: "A description of the second project.",
+    image: "path/to/image2.jpg",
+    url: "https://example.com/project-two",
+  },
+];
+
+export { softwareEngineeringProjects, designProjects, potteryProjects };
