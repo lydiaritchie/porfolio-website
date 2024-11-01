89b17f154f145f8ebaea7fd8e2da01ea638bc499
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-11-01 13:48:04 -0500

    Finished portfolio menu, added image imports

--- src/pages/PortfolioPages/PotteryPortfolio.js
+++ src/pages/PortfolioPages/PotteryPortfolio.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { NavBar } from "../../utils/NavBar";
+import potteryImages from "../../utils/potteryImages";
 
 function PotteryPortfolio() {
-    return (
-        <div className="main-container">
 
-                Pottery!!!!
+    //import images into images and map them into imageGallery
+    const images = potteryImages;
+
+  return (
+    <div className="main-container">
+      <div className="secondary-container text-center">
+        
+        Pottery
+
+        <div>
+
         </div>
-    );
-};
+
+        </div>
+    </div>
+  );
+}
 
 export default PotteryPortfolio;
---
+++ src/utils/potteryImages.js
@@ -0,0 +1,5 @@
+import image1 from "../graphics/pots/portfolio/IMG_2513.png"
+
+export default {
+  image1,
+};
