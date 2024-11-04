8041feccde3996b7a3efd1b5426353e935f0c659
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-11-04 15:44:37 -0600

    added pottery portfolio photos

--- src/pages/PortfolioPages/PotteryPortfolio.js
+++ src/pages/PortfolioPages/PotteryPortfolio.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { NavBar } from "../../utils/NavBar";
-import potteryImages from "../../utils/potteryImages";
+
 
 function PotteryPortfolio() {
 
     //import images into images and map them into imageGallery
-    const images = potteryImages;
+
 
   return (
     <div className="main-container">
--- src/utils/potteryImages.js
+++
@@ -1,5 +0,0 @@
-import image1 from "../graphics/pots/portfolio/IMG_2513.png"
-
-export default {
-  image1,
-};
