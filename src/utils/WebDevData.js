3583c8ce6d674596a699278b087172d2a03b548f
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-11-12 12:34:00 -0600

    Moved pottery photos, added webdev data

---
+++ src/utils/WebDevData.js
@@ -0,0 +1,47 @@
+import restaurantImage from "../graphics/portfolio-projects/software-engineering/restaurant-reservation-screenshot.png";
+import flashcardImage from "../graphics/portfolio-projects/software-engineering/flashcard-home.png";
+import moviesImage from "../graphics/portfolio-projects/software-engineering/movies-screenshot.png";
+import grubdashImage from "../graphics/portfolio-projects/software-engineering/grubdash-screenshot.png";
+import WebDevPortfolio from "../pages/PortfolioPages/WebDevPortfolio";
+
+const webDevData = [
+    {
+      id: 1,
+      title: "Restaurant Reservation System",
+      image: restaurantImage,
+      description: "Fullstack project to create, edit and manage both reservations and tables at a restaurant.",
+      tech: "",
+      link: "https://restaurant-reservation-system-503t.onrender.com",
+      github: "https://github.com/lydiaritchie/restaurant-reservation-system",
+    },
+    {
+        id: 2,
+        title: "Flashcard Application",
+        image: flashcardImage,
+        description: "Web application to create, edit, and delete decks of flashcards.",
+        tech: "",
+        link: "https://flashcard-application-dwl6.onrender.com/",
+        github: "https://github.com/lydiaritchie/flashcard-application",
+      },
+      {
+        id: 3,
+        title: "WeLoveMovies",
+        image: moviesImage,
+        description: "Backend project using a database to display movies, theaters, and show times. Used frontend template to display.",
+        tech: "",
+        link: "https://starter-movie-front-end-hdsf.onrender.com/",
+        github: "https://github.com/lydiaritchie/movies-backend-project",
+      },
+      {
+        id: 4,
+        title: "Grubdash Server",
+        image: grubdashImage,
+        description: "Backend server to manage orders for example food delivery system Grubdash.",
+        tech: "",
+        link: "https://grubdash-server-project.onrender.com",
+        github: "https://github.com/lydiaritchie/grubdash-server-project",
+      },
+  ];
+  
+
+export default { webDevData };
