f10a366e4815b5449a4f8bda13ed1cbf4a794524
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-11-13 15:16:42 -0600

    Added logo designs to design portfolio

--- src/utils/DesignData.js
+++ src/utils/DesignData.js
@@ -3,12 +3,29 @@ import nemo from "../graphics/portfolio-projects/design/stickers/nemo.png";
 import hornbill from "../graphics/portfolio-projects/design/stickers/hornbill.png";
 import chowBanBenKru from "../graphics/portfolio-projects/design/stickers/chowBanBenKru.png";
 
+import openLogo from "../graphics/portfolio-projects/design/logos/open-logo.png";
+import garuda from "../graphics/portfolio-projects/design/logos/illustrated-garuda.png";
+import mastersThrowdown from "../graphics/portfolio-projects/design/logos/masters-throwdown-logo.png";
+import weightliftingLogo from "../graphics/portfolio-projects/design/logos/weightlifting-logo.png";
+import openyogaLogo from "../graphics/portfolio-projects/design/logos/yoga-logo.png";
+import throwdown2024 from "../graphics/portfolio-projects/design/logos/throwdown-2024.png"
+import throwdown2025 from "../graphics/portfolio-projects/design/logos/throwdown-2025.png"
+
 const designData = {
   illustrations: [
     { id: 1, caption: "", description: "Illustrated elephant sticker.", image: elephant },
     { id: 2, caption: "", description: "Illustrated nemo sticker.", image: nemo },
     { id: 3, caption: "", description: "Illustrated horn bill with scientific and common name.", image: hornbill },
     { id: 4, caption: "The villager is the teacher", description: "Thai writing meaning 'the villager is the teacher' with a small illustrated wooden house.", image: chowBanBenKru },
+  ],
+  logos: [
+    { id: 1, caption: "Open Logo", description: "Logo design for Open project.", image: openLogo },
+    { id: 2, caption: "Garuda Illustration", description: "Illustrated Garuda logo.", image: garuda },
+    { id: 3, caption: "Masters Throwdown", description: "Masters Throwdown event logo.", image: mastersThrowdown },
+    { id: 4, caption: "Weightlifting Logo", description: "Logo for weightlifting events.", image: weightliftingLogo },
+    { id: 5, caption: "Open Yoga Logo", description: "Yoga-inspired logo design.", image: openyogaLogo },
+    { id: 6, caption: "Throwdown 2024", description: "Event logo for Throwdown 2024.", image: throwdown2024 },
+    { id: 7, caption: "Throwdown 2025", description: "Event logo for Throwdown 2025.", image: throwdown2025 },
   ],
 };
 
