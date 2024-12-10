e5f6f66d7e66728b3004d08c2fdcdd33b7dadf44
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-12-09 22:08:13 -0600

    Added video production section to design portfolio

--- src/pages/PortfolioPages/DesignPortfolio.js
+++ src/pages/PortfolioPages/DesignPortfolio.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import { Modal, Carousel } from "react-bootstrap";
-import designData from "../../utils/data/DesignData";
+import { designData } from "../../utils/data/DesignData";
+import { videoData } from "../../utils/data/DesignData";
 
 function DesignPortfolio() {
   const [showModal, setShowModal] = useState(false);
   const [activeIndex, setActiveIndex] = useState(0);
 
   const allImages = Object.entries(designData).flatMap(([category, items]) =>
+    items.map((item, index) => ({
+      ...item,
+      category,
+      index,
+    }))
+  );
+
+  const allVideos = Object.entries(videoData).flatMap(([category, items]) =>
     items.map((item, index) => ({
       ...item,
       category,
@@ -28,93 +37,128 @@ function DesignPortfolio() {
   };
 
   const capitalizeCategory = (category) => {
-    return category === "additionalWorks"
-      ? category.charAt(0).toUpperCase() +
-          category.slice(1, 10) +
-          " " +
-          category.slice(10)
-      : category.charAt(0).toUpperCase() + category.slice(1);
+    return category.charAt(0).toUpperCase() + category.slice(1);
   };
 
   return (
     <div>
-      {Object.entries(designData).map(([category, items]) => (
-        <div key={category} className="category-section">
-          <h4 className="heading-italic mt-4">{capitalizeCategory(category)}</h4>
-          <div className="design-container">
-            {items.map((item, index) => {
-              const flatIndex = allImages.findIndex(
-                (img) => img.category === category && img.index === index
-              );
-              return (
-                <div key={item.id} className="design-item" style={{
-                  padding: 0, // Remove any padding from the container
-                  overflow: "hidden", // Prevents the scaled image from overflowing outside its container
-                }}>
-                  <img
-                    src={item.image}
-                    alt={item.description}
-                    loading="lazy"
-                    className="design-img"
-                    onClick={() => handleImageClick(flatIndex)}
-                    style={{ transform: `scale(${item.scale})`, width: "100%", display: "block",}}
-                  />
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      ))}
-
-      <Modal
-        show={showModal}
-        onHide={handleClose}
-        size="lg"
-        centered
-        dialogClassName="modal-90w"
-        className="90w"
-      >
-        <Modal.Body
-          className="p-0"
-          style={{ maxHeight: "90vh", overflow: "hidden" }}
-        >
-          <Carousel
-            activeIndex={activeIndex}
-            onSelect={handleCarouselSelect}
-            interval={null}
-            indicators={false}
-            controls={true}
-            slide
-          >
-            {allImages.map((item, idx) => (
-              <Carousel.Item key={idx}>
-                <div
-                  className=""
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    width: "100%",
-                    height: "100%",
-                    backgroundColor: "black",
-                  }}
-                >
-                  <img
-                    className="d-block"
-                    src={item.image}
-                    alt={item.description}
+      <div>
+        {Object.entries(designData).map(([category, items]) => (
+          <div key={category} className="category-section">
+            <h4 className="heading-italic mt-4 mb-2">
+              {capitalizeCategory(category)}
+            </h4>
+            <div className="design-container">
+              {items.map((item, index) => {
+                const flatIndex = allImages.findIndex(
+                  (img) => img.category === category && img.index === index
+                );
+                return (
+                  <div
+                    key={item.id}
+                    className="design-item"
                     style={{
-                      maxWidth: "100%",
-                      maxHeight: "90vh",
-                      objectFit: "contain",
+                      padding: 0, // Remove any padding from the container
+                      overflow: "hidden", // Prevents the scaled image from overflowing outside its container
                     }}
-                  />
+                  >
+                    <img
+                      src={item.image}
+                      alt={item.description}
+                      className="design-img"
+                      onClick={() => handleImageClick(flatIndex)}
+                      style={{
+                        transform: `scale(${item.scale})`,
+                        width: "100%",
+                        display: "block",
+                      }}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        ))}
+
+        <Modal
+          show={showModal}
+          onHide={handleClose}
+          size="lg"
+          centered
+          dialogClassName="modal-90w"
+          className="90w"
+        >
+          <Modal.Body
+            className="p-0"
+            style={{ maxHeight: "90vh", overflow: "hidden" }}
+          >
+            <Carousel
+              activeIndex={activeIndex}
+              onSelect={handleCarouselSelect}
+              interval={null}
+              indicators={false}
+              controls={true}
+              slide
+            >
+              {allImages.map((item, idx) => (
+                <Carousel.Item key={idx}>
+                  <div
+                    className=""
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      width: "100%",
+                      height: "100%",
+                      backgroundColor: "black",
+                    }}
+                  >
+                    <img
+                      className="d-block"
+                      src={item.image}
+                      alt={item.description}
+                      style={{
+                        maxWidth: "100%",
+                        maxHeight: "90vh",
+                        objectFit: "contain",
+                      }}
+                    />
+                  </div>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </Modal.Body>
+        </Modal>
+      </div>
+
+      <div className="mt-4">
+        <h4 className="heading-italic">Video Production</h4>
+        <div className="mb-3 caption">
+          Serving as the complete media production team, I developed, filmed,
+          and edited these projects. Select archival B-roll complemented my
+          original content.{" "}
+        </div>
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+          {allVideos.map((item, index) => (
+            <div className="col">
+              <div className="video-container">
+                <div key={index} className="ratio ratio-16x9">
+                  <iframe
+                    className="rounded-1"
+                    src={item.link}
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin"
+                    allowfullscreen
+                  ></iframe>
                 </div>
-              </Carousel.Item>
-            ))}
-          </Carousel>
-        </Modal.Body>
-      </Modal>
+
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/utils/data/DesignData.js
+++ src/utils/data/DesignData.js
@@ -12,7 +12,14 @@ import openyogaLogo from "../../graphics/portfolio-projects/design/logos/yoga-lo
 import throwdown2024 from "../../graphics/portfolio-projects/design/logos/throwdown-2024.png";
 import throwdown2025 from "../../graphics/portfolio-projects/design/logos/throwdown-2025.png";
 
-const designData = {
+import throwdownVideo from "../../graphics/portfolio-projects/video/throwdown.jpg";
+import throwdownDay1Video from "../../graphics/portfolio-projects/video/throwdown-day1.jpg";
+import ccc from "../../graphics/portfolio-projects/video/CCC.jpg";
+import ifrp from "../../graphics/portfolio-projects/video/IFRP.jpg";
+import uwl from "../../graphics/portfolio-projects/video/UWL.jpg";
+import samanWod from "../../graphics/portfolio-projects/video/saman-wod.jpg";
+
+export const designData = {
   illustrations: [
     { id: 1, caption: "", description: "Illustrated horn bill with scientific and common name.", image: hornbill },
     { id: 2, caption: "", description: "Illustrated nemo sticker.", image: nemo },
@@ -21,15 +28,31 @@ const designData = {
     { id: 5, caption: "The villager is the teacher", description: "Thai writing meaning 'the villager is the teacher' with a small illustrated wooden house.", image: chowBanBenKru  },
    
   ],
-  logos: [
+  graphics: [
     { id: 1, caption: "Open Logo", description: "Logo design for Open project.", image: openLogo },
     { id: 2, caption: "Garuda Illustration", description: "Illustrated Garuda logo.", image: garuda },
     { id: 3, caption: "Masters Throwdown", description: "Masters Throwdown event logo.", image: mastersThrowdown },
     { id: 4, caption: "Weightlifting Logo", description: "Logo for weightlifting events.", image: weightliftingLogo },
-    { id: 5, caption: "Open Yoga Logo", description: "Yoga-inspired logo design.", image: openyogaLogo },
+    { id: 5, caption: "Yoga Logo", description: "Yoga-inspired logo design.", image: openyogaLogo },
     { id: 6, caption: "Throwdown 2024", description: "Event logo for Throwdown 2024.", image: throwdown2024 },
     { id: 7, caption: "Throwdown 2025", description: "Event logo for Throwdown 2025.", image: throwdown2025 },
   ],
 };
 
-export default designData;
+export const videoData = {
+videoProduction: [
+  { id: 1, caption: "CFCNX Throwdown 2024 Video", description: "Video recap of Throwdown 2024 event at Crossfit Chiang Mai.", 
+    image: throwdownVideo, link: "https://www.youtube.com/embed/3kwT6BpNM4k?si=Mbrqqryl7fvotm_o"},
+  { id: 2, caption: "CFCNX Throwdown 2024 Day 1 Video", description: "Video recap of day 1 of the Throwdown 2024 event at Crossfit Chiang Mai.", 
+    image: throwdownDay1Video, link:"https://www.youtube.com/embed/sBJj_6gBTZc?si=fpGgbzFL20cirrCq" },
+  { id: 3, caption: "Saman WOD event video", description: "Video recap of the Saman WOD and its meaning at Crossfit Chiang Mai.", 
+    image: samanWod, link:"https://www.youtube.com/embed/GGM6olKoPJU?si=tkUaXhu0oCsO5GQD" },
+  { id: 4, caption: "ISDSI IFRP Project Overview", description: "ISDSI students and staff share about Independent Field Reserach Projects that are a part of the CEC semester program.", 
+    image: ifrp, link: "https://www.youtube.com/embed/EHSgrDKEVP0?si=8hyhbzRtFMwrSXJs" },
+  { id: 5, caption: "ISDSI CCC Program Overview", description: "Overview of CCC program content and deadlines at ISDSI.", 
+    image: ccc, link:"https://www.youtube.com/embed/cr0OxdVA5Qk?si=pX9PN7KuL1PKLYWm" },
+  { id: 6, caption: "ISDSI Summer Program Overview", description: "Recap of summer program with UWL students.", 
+    image: uwl, link: "https://www.youtube.com/embed/kLOU8sHpkOE?si=WprUF51nrdJR0cKU"},
+   ] }
+
+
